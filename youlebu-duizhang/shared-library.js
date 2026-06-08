bb20a13fa80e6a114ceed8676483ebe9393a8807
sunlizhu521-alt <sunlizhu521@gmail.com>
2026-06-09 06:40:33 +0800

    Update Youlebu reconciliation logic

--- youlebu-duizhang/shared-library.js
+++ youlebu-duizhang/shared-library.js
@@ -1,5 +1,5 @@
 (function () {
-  const DB_NAME = "youlebu-reconciliation-library";
+  const DB_NAME = "yile-reconciliation-library";
   const DB_VERSION = 1;
   const STORE_NAME = "file-slots";
   const SHARED_LIBRARY_URL = "./data/shared-library.json";
